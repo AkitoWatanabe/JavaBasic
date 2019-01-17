f1e7023e4d6687a24d5ffc3cdebdb317950b5ee5
AkitoWatanabe <akito.watanabe.likeit@gmail.com>
2019-01-17 11:50:54 +0900

    12_01

---
+++ Practice/src/Practice/practice19/Javadam.java
@@ -0,0 +1,11 @@
+package Practice.practice19;
+
+public class Javadam extends Robot{
+	public Javadam(){
+		super.pilot = "ジャムロ";
+	}
+	public void start(){
+		System.out.println(super.pilot + "行きます！");
+	}
+
+}
---
+++ Practice/src/Practice/practice19/Robot.java
@@ -0,0 +1,10 @@
+package Practice.practice19;
+
+public class Robot {
+	public String pilot;
+
+	public void punch(){
+		System.out.println("パンチ！");
+	}
+
+}
