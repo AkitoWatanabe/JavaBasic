8ea09e19170b613e31609c70e255c51f81397def
AkitoWatanabe <akito.watanabe.likeit@gmail.com>
2019-01-18 16:11:22 +0900

    17_04

---
+++ Practice/src/Practice/practice21/Sample.java
@@ -0,0 +1,18 @@
+package Practice.practice21;
+
+public class Sample {
+	static int numCheck(int num){
+			if(num < 0){
+				throw new RuntimeException();
+			}
+			return num;
+	}
+	public static void main(String[] args) {
+		try {
+			System.out.println(numCheck(10));
+			System.out.println(numCheck(-2));
+		}catch(RuntimeException e){
+			System.out.println("RuntimeExceptionが発生しました");
+		}
+	}
+}
--- Practice/src/practice17/ptra17/PTra17_04.java
+++ Practice/src/practice17/ptra17/PTra17_04.java
@@ -10,11 +10,24 @@ public class PTra17_04 {
 	public static void main(String[] args) {
 
 		String str = "スッキリわかるJava入門";
+		int time = 1000;
+		sleepStr(str, time);
+	}
 
 		/*
 		 * ★ strに入っている文字列を1文字ずつゆっくり表示させるプログラムを完成させてください
 		 * ※ Thread.sleep を使用すること
 		 */
-
+	static void sleepStr(String words, int timer) {
+		String base = words;
+		char[] line = base.toCharArray();
+		for(char l:line) {
+			System.out.print(l);
+			try {
+				Thread.sleep(timer);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
