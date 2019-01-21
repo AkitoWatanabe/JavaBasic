09a30795033c94886e64251a98b5db2ca56c32d3
AkitoWatanabe <akito.watanabe.likeit@gmail.com>
2019-01-21 11:17:16 +0900

    18_04

--- Practice/src/practice18/PTra18_04.java
+++ Practice/src/practice18/PTra18_04.java
@@ -9,6 +9,7 @@ package practice18;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 import practice18.entity.Player;
@@ -58,38 +59,118 @@ public class PTra18_04 {
 				String temp = i.getPosition();
 
 
-					if((temp.equals("GK"))){
-						player.setPosition(i.getPosition());
-		                player.setName(i.getName());
-						player.setCountry(i.getCountry());
-						player.setTeam(i.getTeam());
-						gk.add(player);
+				if((temp.equals("GK"))){
+					player.setPosition(i.getPosition());
+					player.setName(i.getName());
+					player.setCountry(i.getCountry());
+					player.setTeam(i.getTeam());
+					gk.add(player);
 
-					}else if(temp.equals("DF")){
-						player.setPosition(i.getPosition());
-		                player.setName(i.getName());
-						player.setCountry(i.getCountry());
-						player.setTeam(i.getTeam());
-						df.add(player);
-					}else if(temp.equals("MF")){
-						player.setPosition(i.getPosition());
-		                player.setName(i.getName());
-						player.setCountry(i.getCountry());
-						player.setTeam(i.getTeam());
-						mf.add(player);
-					}else{
-						player.setPosition(i.getPosition());
-		                player.setName(i.getName());
-						player.setCountry(i.getCountry());
-						player.setTeam(i.getTeam());
-						fw.add(player);
-					}
+				}else if(temp.equals("DF")){
+					player.setPosition(i.getPosition());
+	                player.setName(i.getName());
+	                player.setCountry(i.getCountry());
+					player.setTeam(i.getTeam());
+					df.add(player);
+				}else if(temp.equals("MF")){
+					player.setPosition(i.getPosition());
+	                player.setName(i.getName());
+					player.setCountry(i.getCountry());
+					player.setTeam(i.getTeam());
+					mf.add(player);
+				}else{
+					player.setPosition(i.getPosition());
+	                player.setName(i.getName());
+					player.setCountry(i.getCountry());
+					player.setTeam(i.getTeam());
+					fw.add(player);
 				}
+			}
 
-				for(Player i:array2) {
-						System.out.println(i.toString());
+			int startingmenber;
+			int random;
+			int maxmenber;
+			int[] temp = new int[4];
+			Random rnd = new Random();
+
+			maxmenber = 1;
+			startingmenber = 0;
+			temp[0] = 100;
+			temp[1] = 100;
+			temp[2] = 100;
+			temp[3] = 100;
+			while(true) {
+				if(startingmenber  == maxmenber) {
+					break;
 				}
+				random = rnd.nextInt(gk.size());
+				//System.out.println(gk.size());
+				//System.out.println(random);
 
+				if(temp[startingmenber] != random) {
+					System.out.println(gk.get(random));
+					startingmenber ++;
+				}
+			}
+
+			maxmenber = 4;
+			startingmenber = 0;
+			temp[0] = 100;
+			temp[1] = 100;
+			temp[2] = 100;
+			temp[3] = 100;
+			while(true) {
+				if(startingmenber  == maxmenber) {
+					break;
+				}
+				random = rnd.nextInt(df.size());
+				//System.out.println(df.size());
+				//System.out.println(random);
+
+				if(temp[startingmenber] != random) {
+					System.out.println(df.get(random));
+					startingmenber ++;
+				}
+			}
+
+			maxmenber = 4;
+			startingmenber = 0;
+			temp[0] = 100;
+			temp[1] = 100;
+			temp[2] = 100;
+			temp[3] = 100;
+			while(true) {
+				if(startingmenber  == maxmenber) {
+					break;
+				}
+				random = rnd.nextInt(mf.size());
+				//System.out.println(mf.size());
+				//System.out.println(random);
+
+				if(temp[startingmenber] != random) {
+					System.out.println(mf.get(random));
+					startingmenber ++;
+				}
+			}
+
+			maxmenber = 2;
+			startingmenber = 0;
+			temp[0] = 100;
+			temp[1] = 100;
+			temp[2] = 100;
+			temp[3] = 100;
+			while(true) {
+				if(startingmenber  == maxmenber) {
+					break;
+				}
+				random = rnd.nextInt(fw.size());
+				//System.out.println(fw.size());
+				//System.out.println(random);
+
+				if(temp[startingmenber] != random) {
+					System.out.println(fw.get(random));
+					startingmenber ++;
+				}
 			}
 
 	}
